6668b8efa3781dc319939e356ed337e362e15b7e
johan magnusson <johan.magnusson@optidev.se>
2021-08-03 19:26:18 +0200

    add DB models

---
+++ src/models/models.py
@@ -0,0 +1,18 @@
+from tortoise.models import Model
+from tortoise import fields
+
+class File(Model):
+    file_name = fields.TextField(pk=True, null=False)
+    file_type_extension = fields.TextField(null=False)
+    description = fields.TextField(null=False)
+    uploaded_by = fields.TextField(null=False)
+    uploaded_at = fields.DateField(null=False, default='1992-01-01')
+    
+    class Meta:
+            table='Files'
+            unique_together=( ("file_name", "file_type_extension") )
+
+    def __str__(self):
+        return self.name
+
+
